fa903d9458009b487a8c0cd37cc2a4828abfc877
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2022-06-04 12:01:34 -0400

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Omar Hegazy on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
